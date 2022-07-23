9fce5290019123849543aa927637c6bfa326be68
Virginie PICH <red_iv@hotmail.fr>
2022-07-23 15:54:05 +0200

    Revert "rajout des __str__ dans les classes Player et Match"
    This reverts commit f2aad14b94119911bf63c835bc805f2c75a5ba74.

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from models import Tournament, Player, Round
 
 
-def tournament_controller():
+def tournament_view():
     print("Un nouveau tournoi va commencer !")
-    # name = input("Entrez le nom du tournoi: ")
-    # place = input("Entrez le lieu où se déroule le tournoi: ")
-    # date = input("Entrez la date du tournoi: ")
-    # time_control = input("Entrez le type de contrôle de temps (bullet, blitz ou coup rapide): ")
-    # number_of_rounds = int(input("Entrez le nombre de tours: "))
-    # new_tournament = Tournament(name, place, date, time_control, number_of_rounds)
-    new_tournament = Tournament("name", "place", "date", "time_control", 4)
-    # description = input("Entrez la description du tournoi: ")
-    # new_tournament.description = description
-    print("Tournoi bien créé")
+    name = input("Entrez le nom du tournoi: ")
+    place = input("Entrez le lieu où se déroule le tournoi: ")
+    date = input("Entrez la date du tournoi: ")
+    time_control = input("Entrez le type de contrôle de temps (bullet, blitz ou coup rapide): ")
+    number_of_rounds = int(input("Entrez le nombre de tours: "))
+    new_tournament = Tournament(name, place, date, time_control, number_of_rounds)
+    description = input("Entrez la description du tournoi: ")
+    new_tournament.description = description
     return new_tournament
+
+
+tournament = tournament_view()
 
 
 def main_view():
@@ -23,14 +24,14 @@ def main_view():
     print("3/ Afficher le classement des joueurs ?")
 
 
-def main_controller(tournament):
+def main_controller():
     main_view()
-    choice = int(input("Tapez 1, 2 ou 3: "))
+    choice = input("Tapez 1, 2 ou 3: ")
 
     if choice == 1:
-        players_controller(tournament)
+        players_controller()
     elif choice == 2:
-        rounds_view(tournament)
+        rounds_view()
     elif choice == 3:
         # affichage du classement
         pass
@@ -38,7 +39,7 @@ def main_controller(tournament):
         return
 
 
-def players_view(tournament):
+def players_view():
     print("Liste des participants: ")
     for player in tournament.players:
         print(player)
@@ -49,26 +50,24 @@ def players_view(tournament):
     print("4/ Retour en arrière")
 
 
-def players_controller(tournament):
-    print("---Début player controller")
-    players_view(tournament)
-    choice = int(input("Tapez 1, 2, 3 ou 4: "))
+def players_controller():
+    players_view()
+    choice = input("Tapez 1, 2, 3 ou 4: ")
 
     if choice == 1:
         # il faut 8 joueurs
-        add_player_view(tournament)
+        add_player_view()
     elif choice == 2:
-        modify_player_view(tournament)
+        modify_player_view()
     elif choice == 3:
-        delete_player_view(tournament)
+        delete_player_view()
     elif choice == 4:
-        main_controller(tournament)
+        main_controller()
     else:
-        print("Je n'ai pas compris votre choix")
-        players_controller(tournament)
+        return
 
 
-def add_player_view(tournament):
+def add_player_view():
     firstname = input("Entrez le prénom du joueur: ")
     lastname = input("Entrez le nom de famille du joueur: ")
     date_of_birth = input("Entrez la date de naissance du joueur: ")
@@ -76,13 +75,13 @@ def add_player_view(tournament):
     new_player = Player(firstname, lastname, date_of_birth, gender)
     tournament.players.append(new_player)
     print(f"Le joueur {firstname} {lastname} a bien été ajouté au tournoi.")
-    players_controller(tournament)
+    main_controller()
 
 
-def modify_player_view(tournament):
+def modify_player_view():
     for player, i in enumerate(tournament.players):
         print(f"{i}/ Supprimer joueur {player}")
-    choice = int(input("Tapez le numéro du joueur à modifier: "))
+    choice = input("Tapez le numéro du joueur à modifier: ")
     player_to_modify = tournament.players[choice]
     print("Que souhaitez-vous modifier ?")
     print("1/ Son prénom ?")
@@ -90,7 +89,7 @@ def modify_player_view(tournament):
     print("3/ Sa date de naissance ?")
     print("4/ Son sexe ?")
     print("5/ Son score ?")
-    choice = int(input("Tapez le numéro à modifier: "))
+    choice = input("Tapez le numéro à modifier: ")
     if choice == 1:
         firstname = input("Entrez le prénom du joueur: ")
         player_to_modify.firstname = firstname
@@ -109,39 +108,35 @@ def modify_player_view(tournament):
     else:
         return
     print("La modification a été enregistrée.")
-    players_controller(tournament)
+    main_controller()
 
 
-def delete_player_view(tournament):
+def delete_player_view():
     for player, i in enumerate(tournament.players):
         print(f"{i}/ Supprimer joueur {player}")
-    choice = int(input("Tapez le numéro du joueur à supprimer: "))
+    choice = input("Tapez le numéro du joueur à supprimer: ")
     player_to_delete = tournament.players[choice]
     tournament.delete_player(player_to_delete)
-    print("Le joueur a bien été supprimé")
-    players_controller(tournament)
+    main_controller()
 
 
-def rounds_view(tournament):
+def rounds_view():
     print("Liste des rounds du tournoi:")
     for round in tournament.rounds:
         print(round)
     print("------------------")
-    print("1/ Commencer un nouveau round ?")
+    print("1/ Ajouter un round ?")
+    print("2/ Commencer un round ?")
+    print("3/ Terminer un round ?")
     print("4/ Retour en arrière")
 
 
-def rounds_controller(tournament):
-    rounds_view(tournament)
-    choice = int(input("Tapez 1 ou 2: "))
+def rounds_controller():
+    rounds_view()
+    choice = input("Tapez 1 ou 2: ")
 
     if choice == 1:
         new_round = Round("round name") # nom temporaire
-        # lister les rounds existants, si y'en a déjà trois print tournoi terminé
-        # pour le joueur 1 tu regardes la liste des joueurs contre qui il a joué, prendre un des joueurs restants
-        # exemple joueur 1 est avec 3, créer les deux matchs
-        # envoyer vers match_controller
-
         # ajouter les matchs en faisant les pairs de joueurs qui vont s'affronter:
         #     1/ au 1er tour, trier les joueurs selon leur rang
         #     2/ diviser les joueurs en 2 partie, le meilleur de la 1ère partie affronte le 1er de la seconde moitié
@@ -153,15 +148,9 @@ def rounds_controller(tournament):
     elif choice == 2:
         pass
 
-def match_controller():
-    # print(tout les matchs)
-    # modifier le score du quel ?
-    # quel est le score ?
-    # pour le match choisi faire match.add_score_to_winner()
 
 def ranking_view():
     pass
 
 
-tournament = tournament_controller()
-main_controller(tournament)
+main_controller()
