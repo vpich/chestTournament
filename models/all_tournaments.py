abb534a7fc189b2df381c90c0fb46b72af414e41
Virginie PICH <red_iv@hotmail.fr>
2022-08-16 01:02:11 +0200

    Ajout __str__ pour afficher tous les tournois qui composent la liste tournaments

--- models/all_tournaments.py
+++ models/all_tournaments.py
@@ -5,6 +5,11 @@ class AllTournaments:
     def __init__(self):
         self.tournaments = []
 
+    def __str__(self):
+        print("Voici tous les tournois enregistrés: ")
+        for i, tournament in enumerate(self.tournaments):
+            print(f"{i}/ {tournament}")
+
     def add_tournament(self, tournament):
         assert type(tournament) == Tournament
         self.tournaments.append(tournament)
