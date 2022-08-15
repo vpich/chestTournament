72300504b8cac0bf06f1f83fa858d28c5ce4ead4
Virginie PICH <red_iv@hotmail.fr>
2022-08-16 00:55:21 +0200

    Correction de l'attribut des points, ainsi que du __str__ pour afficher les points gagnés au lieu du classement

--- models/player.py
+++ models/player.py
@@ -8,8 +8,7 @@ class Player:
         self.date_of_birth = date_of_birth
         self.gender = gender
         self.rank = rank
-        self.points_gagnes = 0
-        # a la fin de chaque match,
+        self.total_points = 0
 
     def __str__(self):
-        return f"Joueur {self.firstname} {self.lastname}, classement: {self.rank}"
+        return f"Joueur {self.firstname} {self.lastname}, points gagnés: {self.total_points}"
