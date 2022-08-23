d6ddeafb8de9d1b14e4dc2470353e14130238859
Virginie PICH <red_iv@hotmail.fr>
2022-08-23 19:09:10 +0200

    Rajout de la méthode save

--- models/tournament.py
+++ models/tournament.py
@@ -1,8 +1,10 @@
 from .player import Player
+from tinydb import TinyDB, Query
 
 
 class Tournament:
-    def __init__(self, name, place, date, time_control, number_of_rounds: int = 4):
+    def __init__(self, id, name, place, date, time_control, number_of_rounds: int = 4):
+        self.id = id
         self.name = name
         self.place = place
         # la date pourra à l'avenir être plusieurs jours, et pas juste un jour
@@ -43,3 +45,43 @@ class Tournament:
         self.players.reverse()
         # for player in self.players:
         #     print(player)
+
+    def save(self):
+        db = TinyDB("db.json")
+        data_rounds = []
+        for round in self.rounds:
+            data_matches = []
+            for match in round.matches:
+                data_matches.append({"Type": "Match",
+                                     "Player1": f"{match.contestants[0]}",
+                                     "Player2": f"{match.contestants[1]}",
+                                     "ScorePlayer1": f"{match.scores[0]}",
+                                     "ScorePlayer2": f"{match.scores[1]}",
+                                     "InProgress": f"{match.in_progress}"})
+            data_rounds.append({"Type": "Round",
+                                "Name": f"{round.name}",
+                                "Matches": data_matches})
+        data_players = []
+        for player in self.players:
+            data_players.append({"Type": "Player",
+                                 "Firstname": f"{player.firstname}",
+                                 "Lastname": f"{player.lastname}",
+                                 "Gender": f"{player.gender}",
+                                 "DateOfBirth": f"{player.date_of_birth}",
+                                 "Rank": f"{player.rank}",
+                                 "TotalPoints": f"{player.total_points}"})
+        data_tournoi = {"id": f"{self.id}",
+                        "Name": f"{self.name}",
+                        "Place": f"{self.place}",
+                        "Date": f"{self.date}",
+                        "TimeControl": f"{self.time_control}",
+                        "Rounds": data_rounds,
+                        "Description": f"{self.description}",
+                        "Players": data_players}
+        query = Query()
+        exist = db.search(query.id == str(self.id))
+
+        if exist:
+            db.update(data_tournoi, query.id == f"{self.id}")
+        else:
+            db.insert(data_tournoi)
