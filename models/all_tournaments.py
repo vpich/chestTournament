26788bd314579cc78e87efd6383a080747af545d
Virginie PICH <red_iv@hotmail.fr>
2022-08-23 19:11:41 +0200

    Rajout d'un début de travail avec Sofien qui servira à l'enregistrement des données dans db.json

--- models/all_tournaments.py
+++ models/all_tournaments.py
@@ -4,6 +4,13 @@ from models import Tournament
 class AllTournaments:
     def __init__(self):
         self.tournaments = []
+        all_tournaments_data = db.all()
+        for tournament_data in all_tournaments_data:
+            list_players = []
+            for player_data in tournament_data["Players"]:
+                list_players.append(Player(player_data))
+            tournament = Tournament(tournament_data)
+            self.tournaments.append(tournament)
 
     def __str__(self):
         print("Voici tous les tournois enregistrés: ")
