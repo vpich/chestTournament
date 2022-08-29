785296e05234d27a04b7961382b9a30a2b38eca1
Virginie PICH <red_iv@hotmail.fr>
2022-08-29 19:38:08 +0200

    Suppression d'une partie du code qui n'est plus utilisée

--- models/all_tournaments.py
+++ models/all_tournaments.py
@@ -4,13 +4,6 @@ from models import Tournament
 class AllTournaments:
     def __init__(self):
         self.tournaments = []
-        all_tournaments_data = db.all()
-        for tournament_data in all_tournaments_data:
-            list_players = []
-            for player_data in tournament_data["Players"]:
-                list_players.append(Player(player_data))
-            tournament = Tournament(tournament_data)
-            self.tournaments.append(tournament)
 
     def __str__(self):
         print("Voici tous les tournois enregistrés: ")
